d1f17d765963738d865d582a21ea566627703899
maxADeVisser <m.madv96@gmail.com>
2023-10-25 14:41:50 +0200

    constructing the restricted tree correctly now

--- handins/handin6/practical6.py
+++ handins/handin6/practical6.py
@@ -36,10 +36,25 @@ def build(df: pd.DataFrame) -> nx.DiGraph:
     return G
 
 
+def get_children(tree: nx.DiGraph, node_ID: int) -> list[int]:
+    """Given a tree graph, return a list of the children of the node_ID"""
+    return [c for c, _ in tree.out_edges(node_ID)]
+
+
+def get_parent(tree: nx.DiGraph, node_ID: int) -> int | None:
+    """Given a tree graph, return the parent of the given node"""
+    parent_nodes = [p for p, _ in tree.in_edges(node_ID)]
+    if len(parent_nodes) > 1:
+        raise Exception(f"There are multiple parent nodes for node {node_ID}")
+    if len(parent_nodes) == 0:
+        return None
+    return parent_nodes[0]
+
+
 def restrict_tree(tree: nx.DiGraph, df: pd.DataFrame) -> nx.DiGraph:
-    restricted_tree = total_G.copy()
+    restricted_tree = tree.copy()
     standard_taxonomic_ranks = [
-        "superkingdom",  # is actually kingdom
+        "superkingdom",  # is actually kingdom in the data
         "phylum",
         "class",
         "order",
@@ -48,44 +63,46 @@ def restrict_tree(tree: nx.DiGraph, df: pd.DataFrame) -> nx.DiGraph:
         "species",
     ]
 
+    # list of nodes ID with non-standard taxonomic rank:
     non_standard_nodes = list(
         df[~df["rank"].isin(standard_taxonomic_ranks)]["Node ID"].values
-    )  # list of nodes ID with non-standard taxonomic rank
+    )
+
     no_parent_node_counter = 0
     for node_ID in non_standard_nodes:
-        if parent_nodes := [c for c, _ in restricted_tree.in_edges(node_ID)]:
-            # Check that there in fact only is one parent for each node
-            if len(parent_nodes) > 1:
-                raise Exception(f"There are multiple parent nodes for node {node_ID}")
-
-            parent_node_ID = parent_nodes[0]
-
-            # connect each child node from current node to the parent node of current node
-            for child_node_ID in [
-                child for _, child in restricted_tree.out_edges(node_ID)
-            ]:
+        if parent_node_ID := get_parent(tree, node_ID):
+            children = get_children(tree, node_ID)
+            for child_node_ID in children:
                 restricted_tree.add_edge(parent_node_ID, child_node_ID)
 
-            # remove current node
+            # Remove node and edges connected to it
             restricted_tree.remove_node(node_ID)
         else:
-            # current node has no parents
             no_parent_node_counter += 1
             continue
 
+        # if parent_nodes := [c for c, _ in restricted_tree.in_edges(node_ID)]:
+        #     # Check that there in fact only is one parent for each node
+        #     if len(parent_nodes) > 1:
+        #         raise Exception(f"There are multiple parent nodes for node {node_ID}")
+
+        #     parent_node_ID = parent_nodes[0]
+
+        #     # connect each child node from current node to the parent node of current node
+        #     for child_node_ID in [
+        #         child for _, child in restricted_tree.out_edges(node_ID)
+        #     ]:
+        #         restricted_tree.add_edge(parent_node_ID, child_node_ID)
+
+        #     # remove current node
+        #     restricted_tree.remove_node(node_ID)
+        # else:
+        #     # current node has no parents
+        #     no_parent_node_counter += 1
+        #     continue
+
     print(f"Found {no_parent_node_counter} nodes with no parents")
     return restricted_tree
-
-
-def get_parent(tree: nx.DiGraph, node_ID: int) -> int:
-    parent_nodes = [c for c, _ in restricted_tree.in_edges(node_ID)]
-    if len(parent_nodes) > 1:
-        raise Exception(f"There are multiple parent nodes for node {node_ID}")
-    return parent_nodes[0]
-
-
-def get_children(tree: nx.DiGraph, node_ID: int) -> list[int]:
-    return [c for c, _ in restricted_tree.in_edges(node_ID)]
 
 
 def contract_tree(tree: nx.DiGraph) -> nx.DiGraph:
@@ -93,7 +110,7 @@ def contract_tree(tree: nx.DiGraph) -> nx.DiGraph:
 
     for node in tree.nodes:
         # If there is only one child and parent for the node
-        if tree.in_degree(node) == 1 and tree.out_degree(node) == 1:
+        if tree.this is not (node) == 1 and tree.out_degree(node) == 1:
             tree.add_edge(
                 get_parent(return_tree, node), get_children(return_tree, node)[0]
             )
@@ -119,7 +136,9 @@ def find_lineage(tax_id, df):
     while tax_id != 1:
         node_info = df[df["Node ID"] == tax_id].iloc[0]
         lineage.append((node_info["Node ID"], node_info["rank"]))
-        tax_id = node_info["Parent Node ID"]
+        tax_id = node_info[
+            "Parent Node ID"
+        ]  # update tax_id to the parent node of the current node
 
     # Add the root
     node_info = df[df["Node ID"] == tax_id].iloc[0]
@@ -145,52 +164,53 @@ def find_lca(G, node_list):
 if __name__ == "__main__":
     df = read_nodes("handins/handin6/nodes.dmp")
     total_G = build(df)
-    total_G.remove_edge(1, 1)  # remove the one cycle
-    # print(nx.is_tree(total_G))
-    # print(nx.is_directed_acyclic_graph(total_G))
+    total_G.remove_edge(1, 1)  # remove the one cycle from the first node to it self
+    # nx.is_tree(total_G)
+    # nx.is_directed_acyclic_graph(total_G)
 
     # Restrict the tree
     restricted_tree = restrict_tree(total_G, df)
+    # nx.is_tree(restricted_tree)
+    # nx.is_directed_acyclic_graph(restricted_tree)
 
-    # Find lineages in restricted tree
+    # # Find lineages in restricted tree
+    # mappings = read_mapping("handins/handin6/mapping.txt")
 
-    mappings = read_mapping("handins/handin6/mapping.txt")
+    # # Find lineages
+    # lineages = {}
+    # for sequence_id, tax_ids in mappings.items():
+    #     lineages_for_seq = []
+    #     for tax_id in tax_ids:
+    #         lineage = find_lineage(tax_id, df)
+    #         lineages_for_seq.append(lineage)
+    #     lineages[sequence_id] = lineages_for_seq
 
-    # Find lineages
-    lineages = {}
-    for sequence_id, tax_ids in mappings.items():
-        lineages_for_seq = []
-        for tax_id in tax_ids:
-            lineage = find_lineage(tax_id, df)
-            lineages_for_seq.append(lineage)
-        lineages[sequence_id] = lineages_for_seq
+    # lca_skeleton_tree = {}
+    # for read_id, node_list in mappings.items():
+    #     lca = find_lca(restricted_tree, node_list)
+    #     if lca is not None:
+    #         # Get all nodes from the root to each node in node_list and the LCA
+    #         nodes_to_include = set()
+    #         nodes_to_include.add(1)
+    #         for node in node_list:
+    #             nodes_to_include.update(
+    #                 nx.shortest_path(restricted_tree, source=1, target=node)
+    #             )
+    #             nodes_to_include.update(
+    #                 nx.shortest_path(restricted_tree, source=1, target=lca)
+    #             )
 
-    lca_skeleton_tree = {}
-    for read_id, node_list in mappings.items():
-        lca = find_lca(restricted_tree, node_list)
-        if lca is not None:
-            # Get all nodes from the root to each node in node_list and the LCA
-            nodes_to_include = set()
-            nodes_to_include.add(1)
-            for node in node_list:
-                nodes_to_include.update(
-                    nx.shortest_path(restricted_tree, source=1, target=node)
-                )
-                nodes_to_include.update(
-                    nx.shortest_path(restricted_tree, source=1, target=lca)
-                )
+    #         skeleton_tree = restricted_tree.subgraph(nodes_to_include)
 
-            skeleton_tree = restricted_tree.subgraph(nodes_to_include)
+    #         lca_skeleton_tree[read_id] = skeleton_tree
 
-            lca_skeleton_tree[read_id] = skeleton_tree
+    # # Number of nodes in LCA skeleton tree for each sequences read:
+    # # There should be 16 in the first one
+    # num_nodes_per_read = {}
+    # for read_id, lca_tree in lca_skeleton_tree.items():
+    #     num_nodes = len(lca_tree.nodes())
+    #     num_nodes_per_read[read_id] = num_nodes
 
-    # Number of nodes in LCA skeleton tree for each sequences read:
-    # There should be 16 in the first one
-    num_nodes_per_read = {}
-    for read_id, lca_tree in lca_skeleton_tree.items():
-        num_nodes = len(lca_tree.nodes())
-        num_nodes_per_read[read_id] = num_nodes
-
-    print("Number of nodes in LCA skeleton tree for each sequence read:")
-    for read_id, num_nodes in num_nodes_per_read.items():
-        print(f"{read_id}: {num_nodes} nodes")
+    # print("Number of nodes in LCA skeleton tree for each sequence read:")
+    # for read_id, num_nodes in num_nodes_per_read.items():
+    #     print(f"{read_id}: {num_nodes} nodes")
